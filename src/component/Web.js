d040cc240a38b95cc6775be393c17dcc1cf3ebdc
JuanLicciardi <juanelicciardi@gmail.com>
2022-12-06 23:27:33 -0300

    Finalizado Microdesafio 03

--- src/component/Web.js
+++ src/component/Web.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Encabezado from './Encabezado';
+import Banner from './Banner';
 
 function Web(){
   return (
     <div className="container">
-     <h1>Bienvenido a nuestro primer proyecto en React</h1>
-     <Encabezado/>
+      <h1>Bienvenido a nuestro primer proyecto en React</h1>
+      <Encabezado/>
+      <Banner/>
     </div>
   )
 
